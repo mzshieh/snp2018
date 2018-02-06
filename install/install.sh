855b6d1e3d5c9c3b38f8eef87a8d31f3db35f790
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2018-02-06 13:44:00 +0800

    installation script for manjaro

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,5 @@ sudo pacman -R python-pyqt5
 sudo pacman -Syu
 sudo pacman -S vim hdf5 opencv scrot python-xlib
 sudo mkdir .tmp_pip
-sudo -H pip install -b .tmp_pip spyder numpy pillow mss lxml beautifulsoup4
-sudo -H pip install -b .tmp_pip pyautogui
+sudo -H pip install -b .tmp_pip spyder numpy pillow mss lxml beautifulsoup4 pyautogui
 sudo rm -rf .tmp_pip
