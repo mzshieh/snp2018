8fc9d6c732b9a05f1b2ae3e4d4cb90e38cc77aee
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2018-02-06 16:03:38 +0800

    fix

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo pacman -R python-pyqt5 python-pillow pyqt5-common 
+sudo pacman -R python-pyqt5 pyqt5-common 
 sudo pacman -Syu
 sudo pacman -S vim hdf5 opencv scrot
 sudo pacman -S fcitx-gtk3 fcitx-chewing fcitx-configtool fcitx
@@ -9,5 +9,5 @@ echo XMODIFIERS=@im=fcitx >> ~/.xprofile
 echo alias vi='vim' >> ~/.bashrc
 sudo mkdir .tmp_pip
 sudo -H pip install -b .tmp_pip spyder numpy Xlib
-sudo -H pip install -b .tmp_pip pillow mss lxml beautifulsoup4 pyautogui
+sudo -H pip install -b .tmp_pip requests pillow mss lxml beautifulsoup4 pyautogui
 sudo rm -rf .tmp_pip
