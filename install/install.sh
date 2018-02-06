84e7eae8ab1a3cf84cdd047a6deb52144b1e7481
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2018-02-06 14:50:54 +0800

    install manjaro

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-sudo pacman -R python-pyqt5
+sudo pacman -R python-pyqt5 python-pillow pyqt5-common 
 sudo pacman -Syu
-sudo pacman -S vim hdf5 opencv scrot python-xlib
+sudo pacman -S vim hdf5 opencv scrot
+sudo pacman -S fcitx-gtk3 fcitx-im fcitx-chewing fcitx-configtool fcitx
+echo export GTK_IM_MODULE=fcitx >> ~/.xprofile
+echo export QT_IM_MODULE=fcitx >> ~/.xprofile
+echo XMODIFIERS=@im=fcitx >> ~/.xprofile
+echo alias vi='vim' >> ~/.bashrc
 sudo mkdir .tmp_pip
-sudo -H pip install -b .tmp_pip spyder numpy pillow mss lxml beautifulsoup4 pyautogui
+sudo -H pip install -b .tmp_pip spyder numpy Xlib
+sudo -H pip install -b .tmp_pip pillow mss lxml beautifulsoup4 pyautogui
 sudo rm -rf .tmp_pip
