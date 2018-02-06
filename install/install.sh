15b94621ac6371f159f5e94487e5b76ed9b60b10
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2018-02-06 14:59:12 +0800

    Test

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,7 @@
 sudo pacman -R python-pyqt5 python-pillow pyqt5-common 
 sudo pacman -Syu
 sudo pacman -S vim hdf5 opencv scrot
-sudo pacman -S fcitx-gtk3 fcitx-im fcitx-chewing fcitx-configtool fcitx
+sudo pacman -S fcitx-gtk3 fcitx-chewing fcitx-configtool fcitx
 echo export GTK_IM_MODULE=fcitx >> ~/.xprofile
 echo export QT_IM_MODULE=fcitx >> ~/.xprofile
 echo XMODIFIERS=@im=fcitx >> ~/.xprofile
