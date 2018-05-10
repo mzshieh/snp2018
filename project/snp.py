1966c014d2f15f961188f072ea0ddd01f33614f7
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2018-05-10 14:14:45 +0800

    temp fix

--- project/snp.py
+++ project/snp.py
@@ -57,30 +57,21 @@ from mss import mss
 from PIL import Image
 import cv2 as cv
 import numpy as np
+from pyautogui import size
 
-def screenshot(region=None, **kwargs):
-    im = None
-    monitors = None
-    if region == None:
-        region = kwargs.get('region')
+def screenshot(dino, region):
+    bbox={'left': region[0], 'top': region[1], 'width': region[2], 'height': region[3]} # detect box
     
     with mss() as sct:
-
-        # Region to capture
-        monitor = sct.monitors[1]
-        if region != None:
-            monitor['left'] = int(region[0])
-            monitor['top'] = int(region[1])
-            monitor['width'] = int(region[2])
-            monitor['height'] = int(region[3])
-
-        # Get pixels on image
-        sct_img = sct.grab(monitor)
-        im = Image.frombytes('RGBA', sct_img.size, bytes(sct_img.raw), 'raw', 'BGRA')
-        im = im.convert('RGB')
-    return im
+        mss_im = sct.grab(bbox)
+    im = np.array(mss_im.pixels,np.uint8)
+    im = cv.cvtColor(im, cv.COLOR_RGB2BGR)
+    cv.imshow('bbox', im)
+    result = cv.matchTemplate(im, dino, cv.TM_CCOEFF_NORMED)
+    return result
 
 def locateOnScreen(dino, threshold=0.87, region=(0,0,600,200)):
+    if region == None: region = (0,0)+size()
     if type(dino) is not np.ndarray :
         dino = cv.imread(dino,cv.IMREAD_COLOR) # BGR Color
     
@@ -93,12 +84,14 @@ def locateOnScreen(dino, threshold=0.87, region=(0,0,600,200)):
         return None
 
 def locateCenterOnScreen(dino, threshold=0.87, region=(0,0,600,200)):
+    if region == None: region = (0,0)+size()
     p = locateOnScreen(dino,threshold,region)
     if p != None:
         p = ( p[0]+p[2]//2, p[1]+p[3]//2 )
     return p
 
 def locateAllOnScreen(dino, threshold=0.87, region=(0,0,600,200)):
+    if region == None: region = (0,0)+size()
     if type(dino) is not np.ndarray :
         dino = cv.imread(dino,cv.IMREAD_COLOR) # BGR Color
     
